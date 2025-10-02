c937651fb57cf7c0bdbebef69af3b075d676ec96
Lucas Pamplona <pamplona.developer@gmail.com>
2025-10-01 22:06:29 -0300

    Rotas para produtos

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -13,3 +14,44 @@ def rota_teste():
     return {
         "message": "Tá funcionando"
     }
+    
+    
+    
+class Produto(BaseModel):
+    id: int
+    nome: str
+    
+    
+produtos = []
+    
+@app.get("/produtos")
+def get_produtos():
+    return produtos
+
+@app.post("/produtos")
+def post_produtos(item: Produto):
+    for produto in produtos:
+        if produto.id == item.id:
+            raise HTTPException(status_code=400, detail="Item já cadastrado")
+    
+    produtos.append(item)
+    return item
+
+@app.patch("/produtos/{id}")
+def patch_produtos(item: Produto, id: int):
+    indice_produto = None
+    for i in range(len(produtos)):
+        produto = produtos[i]
+        if produto.id == id:
+            indice_produto = i
+            break
+    
+    if indice_produto is None:
+        raise HTTPException(status_code=404, detail="Produto não encontrado")
+    
+    produtos[indice_produto] = item
+    return item
+
+@app.delete("/produtos/{id}")
+def delete_produtos():
+    return "delete_produtos"
